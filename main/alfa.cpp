d227b3d9020d5ec5207ef1669ddf12c223675176
quim-chegalume <joaquimvazp@gmail.com>
2023-06-04 15:27:53 +0100

    am

--- main/alfa.cpp
+++ main/alfa.cpp
@@ -5,6 +5,7 @@
 #include "TCanvas.h"
 #include "TApplication.h"
 #include "TF1.h"
+#include "TAxis.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -68,7 +69,7 @@ int main()
     TCanvas c("canvas", "grafico", 200, 10, 1920, 1080);
     
     // Criar o objeto 
-    TH1F *h1 = new TH1F("h1", "histograma", 1024, 0, 1023);
+    TH1F *h1 = new TH1F("h1", "Espetro de emissao alfa do americio", 1024, 0, 1023);
 
     for (int i = 1; i < N; i++)
     {
@@ -76,19 +77,25 @@ int main()
         h1->Fill(channels[i], contagens[i]);
     }
     h1->SetLineWidth(0);
-    h1->SetMarkerStyle(5);
+    h1->SetMarkerStyle(20);
+    h1->SetMarkerSize(1);
+    h1->SetMarkerColor(13);
 
     TF1 *f1 = new TF1("f1", "gaus", 420, 440);
     f1->SetLineColor(kRed);
+    f1->SetLineStyle(9);
     
     TF1 *f2 = new TF1("f2", "gaus", 450, 470);
     f2->SetLineColor(kBlue);
+    f2->SetLineStyle(9);
 
     TF1 *f3 = new TF1("f3", "gaus", 470, 510);
     f3->SetLineColor(kPink);
+    f3->SetLineStyle(9);
 
     TF1 *total = new TF1("total", "gaus(0) + gaus(3) + gaus(6)", 410, 510);
     total->SetLineColor(kBlack);
+    total->SetLineWidth(4);
 
     h1->Fit(f1, "WR");
     h1->Fit(f2, "WR+");
@@ -100,6 +107,11 @@ int main()
     total->SetParameters(par);
 
     h1->Fit(total, "WR+");
+
+    h1 ->GetXaxis()->SetRange(410,510);
+    h1 ->GetXaxis()->SetTitle("Canais");
+    h1 ->GetYaxis()->SetRangeUser(0,1500);
+    h1 ->GetYaxis()->SetTitle("Eventos");
 
     c.Update();
     h1->Draw();
