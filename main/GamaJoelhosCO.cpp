17b3f39f767639417bd0d96d12928edd0713473b
quim-chegalume <joaquimvazp@gmail.com>
2023-06-09 14:04:38 +0100

    gama2

--- main/GamaJoelhosCO.cpp
+++ main/GamaJoelhosCO.cpp
@@ -67,7 +67,7 @@ int main()
 
     double energias[N];
     double er_contagens[N];
-    double er_energias[N]; fill_n(er_energias, N, 0.009);
+    double er_energias[N]; fill_n(er_energias, N, 1.701);
 
     for (int i = 0; i < N; i++)
     {
@@ -84,34 +84,34 @@ int main()
     // Criar o objeto 
     auto h1 = new TGraphErrors(N, energias, contagens, er_energias, er_contagens);
     h1->SetMarkerStyle(3); h1->SetMarkerSize(1);
-    h1->SetTitle("Patamar de Compton do Cobalto");
+    h1->SetTitle("Joelhos de Compton no Cobalto");
 
 
-    // TF1 *f1 = new TF1("f1", "gaus", 0, round(energy(1023)));
-    // f1->SetLineColor(kRed);f1->SetLineWidth(3);
+    TF1 *f1 = new TF1("f1", "gaus", 0, round(energy(1023)));
+    f1->SetLineColor(kRed);f1->SetLineWidth(3);
     
-    // TF1 *f2 = new TF1("f2", "gaus", 0, round(energy(1023)));
-    // f2->SetLineColor(kBlue);f2->SetLineWidth(3);
+    TF1 *f2 = new TF1("f2", "gaus", 0, round(energy(1023)));
+    f2->SetLineColor(kBlue);f2->SetLineWidth(3);
 
 
 
-    // h1->Fit(f1, "W", "", 179, 217);
-    // auto *f1C = new TF1(*f1);
-    // f1C->SetLineStyle(9);f1C->SetLineWidth(2);
+    h1->Fit(f1, "W", "", 815, 975);
+    auto *f1C = new TF1(*f1);
+    f1C->SetLineStyle(9);f1C->SetLineWidth(2);
 
-    // h1->Fit(f2, "W+", "", 440, 500);
-    // auto *f2C = new TF1(*f2);
-    // f2C->SetLineStyle(9);f2C->SetLineWidth(2);
+    h1->Fit(f2, "W+", "", 1000, 1100);
+    auto *f2C = new TF1(*f2);
+    f2C->SetLineStyle(9);f2C->SetLineWidth(2);
 
     //eixos
-    // h1 ->GetXaxis()->SetRangeUser(150,600);
+    h1 ->GetXaxis()->SetRangeUser(750,1150);
     h1 ->GetXaxis()->SetTitle("Energia (KeV)");
-    // h1 ->GetYaxis()->SetRangeUser(0,2500);
+    h1 ->GetYaxis()->SetRangeUser(0,140);
     h1 ->GetYaxis()->SetTitle("Contagens");
     c.Update();
     h1->Draw("AP");
-    // f1C->Draw("Same");f2C->Draw("Same");
-    c.SaveAs("comptoncesio.png");
+    f1C->Draw("Same");f2C->Draw("Same");
+    c.SaveAs("comptoncobalto.png");
     c.WaitPrimitive();
 
     App.Run();
